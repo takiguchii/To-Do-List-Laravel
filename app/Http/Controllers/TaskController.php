f92419da39d022654225f0609230ee97d21ac1ca
Takiguchii <alexandre.pupim360@gmail.com>
2025-07-13 19:08:04 -0300

    Feat: Create task control

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+use App\Repositories\TaskRepository;
+
+class TaskController extends Controller
+{
+    protected $repo;
+
+    public function __construct()
+    {
+        $this->repo = new TaskRepository();
+    }
+
+    public function index()
+    {
+        $tasks = $this->repo->all();
+        return view('tasks.index', compact('tasks'));
+    }
+
+    public function store(Request $request)
+    {
+        $id = uniqid(); // Gera um ID único
+        $title = $request->input('title');
+        $task = new Task($id, $title);
+        $this->repo->save($task);
+
+        return redirect()->route('tasks.index');
+    }
+
+    public function destroy($id)
+    {
+        $this->repo->delete($id);
+        return redirect()->route('tasks.index');
+    }
+}
